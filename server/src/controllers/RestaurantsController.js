b3ef7a142a8a8daf544d00327f2cb346794d46d3
HigorAntonio <higorantonio.ha@gmail.com>
2021-01-22 19:04:14 -0300

    Get a Restaurant Route

--- server/src/controllers/RestaurantsController.js
+++ server/src/controllers/RestaurantsController.js
@@ -1,11 +1,14 @@
+const { restart } = require('nodemon');
 const knex = require('../database');
 
 module.exports = {
   async index(req, res) {
     try {
-      const restaurants = await knex('restaurants');
-      return res.json({
-        status: "success",
+      const restaurants = await knex('restaurants')
+        .select('id', 'name', 'location', 'price_range');
+      
+        return res.json({
+        status: 'success',
         results: restaurants.length,
         data: {
           restaurants
@@ -16,13 +19,32 @@ module.exports = {
     }
   },
 
-  show(req, res) {
-    res.json({
-      status: 'success',
-      data: {
-        restaurant: 'mcdonalds'
+  async show(req, res) {
+    try {
+      const restaurantId = req.params.id;
+      
+      if (!Number(restaurantId)) {
+        return res.sendStatus(404);
       }
-    });
+
+      const restaurant = await knex('restaurants')
+        .select('id', 'name', 'location', 'price_range')
+        .where({ id: restaurantId })
+        .first();
+
+      if (!restaurant) {
+        return res.status(404).json({ erro: 'Usuário não encontrado' });
+      }
+      
+      return res.json({
+        status: 'success',
+        data: {
+          restaurant
+        }
+      })
+    } catch (error) {
+      return res.sendStatus(500);      
+    }
   },
 
   create(req, res) {
