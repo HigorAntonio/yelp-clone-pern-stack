269bc2551e9eade4f0bc94b72b60b5f06e4cb18e
HigorAntonio <higorantonio.ha@gmail.com>
2021-01-22 21:59:20 -0300

    React Router Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdateRestaurantPage from './routes/UpdateRestaurantPage';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/restaurants/:id/update' component={UpdateRestaurantPage} />
+          <Route exact path='/restaurants/:id' component={RestaurantDetailPage} />
+        </Switch>
+      </Router>
+    </div>
   )
 };
 
---
+++ client/src/routes/UpdateRestaurantPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateRestaurantPage = () => {
+  return (
+    <div>
+      UpdateRestaurantPage
+    </div>
+  )
+}
+
+export default UpdateRestaurantPage
